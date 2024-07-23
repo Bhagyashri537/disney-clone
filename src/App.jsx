b1c5c421b903722ba0eb8ce00671b6f27edba463
Bhagyashri <bhagyashrideoghare116@gmail.com>
2024-07-23 18:31:01 +0530

    header is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
+import Header from './components/Header'
+
 
 function App() {
   
   return (
-    <div>
-     <h1 className="text-3xl text-orange-700 font-bold underline">
-      Hello world!
-    </h1>
+    <div className=''>
+    <Header/>
     </div>
   )
 }
